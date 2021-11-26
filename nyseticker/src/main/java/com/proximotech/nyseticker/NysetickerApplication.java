540f5973ab2a8174eab4b4083864a51b179906cd
konzern <kv.nijesh@gmail.com>
2021-11-27 01:23:11 +0530

    base structure

---
+++ nyseticker/src/main/java/com/proximotech/nyseticker/NysetickerApplication.java
@@ -0,0 +1,13 @@
+package com.proximotech.nyseticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NysetickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NysetickerApplication.class, args);
+	}
+
+}
---
+++ nyseticker/src/test/java/com/proximotech/nyseticker/NysetickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proximotech.nyseticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NysetickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
